779e4ee15e81a15b7334e918fe4b823df8f694f2
Saurabh Goyal <sgoyal@ch.iitr.ac.in>
2021-08-05 13:24:20 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import "./styles.css";
+import React from "react";
+import Review from "./Review";
+
+export default function App() {
+  return (
+    <main>
+      <section className="container">
+        <div className="title">
+          <h2>our reviews</h2>
+          <div className="underline"></div>
+        </div>
+        <Review />
+      </section>
+    </main>
+  );
+}
